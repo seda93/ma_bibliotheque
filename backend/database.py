2892e0978969903ee7440cc6b13894e42518f246
seda93 <seda.ozturk39@gmail.com>
2025-07-18 13:28:36 +0200

    Correction backend

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
+import os
 import streamlit as st
 
+# 🔐 Fonction pour obtenir une connexion SQLAlchemy à Supabase (PostgreSQL)
 def get_sqlalchemy_engine():
     return create_engine(
-        f"postgresql+psycopg2://{st.secrets['user']}:{st.secrets['password']}@{st.secrets['host']}:{st.secrets['port']}/{st.secrets['dbname']}"
+        f"postgresql://{st.secrets['user']}:{st.secrets['password']}@{st.secrets['host']}:{st.secrets['port']}/{st.secrets['dbname']}",
+        connect_args={"sslmode": "require"}
     )
